4ead99fb599a069ecc24e6c9d0eade2b5341cbc5
CasianLW <ciorbcasian@gmail.com>
2023-02-17 12:17:57 +0100

    maj liens menu components

--- components/shared/MenuComponent.js
+++ components/shared/MenuComponent.js
@@ -1,6 +1,5 @@
 const MenuComponent = {
-  template:
-    '<div><router-link to="/">Go to Home</router-link> <br> <router-link to="/locations">See locations</router-link></div>',
+  template: '<div><router-link to="/">Go to Home</router-link> <br> ',
 };
 
 export default MenuComponent;
