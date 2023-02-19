8e0463871b39f40b261f99c9d8e2d91e31d277ff
CasianLW <ciorbcasian@gmail.com>
2023-02-19 17:29:10 +0100

    correction toggle visited

--- components/LocationPage.js
+++ components/LocationPage.js
@@ -2,7 +2,7 @@ import { API_URL } from "../config.js";
 
 const LocationPage = {
   template:
-    "<div >Welcome to the {{ location.name }} Page!</div> <br> <br>       <form @submit.prevent='addPlace()'><h2>Add new places</h2> <div style='display:flex;flex-direction: column;'><label for='name'>Place Name</label><input name='name' v-model='name' type='text' /></div><div class='lat-long'><div><label for='latitude'>Latitude</label><input name='lat' v-model='lat' type='number' /></div><div><label for='latitude'>Longitude</label><input name='lng' v-model='lng' type='number' /></div></div><button class='btn-add' type='submit'>Add new place</button></form>   <br> <br>   <div class='empty-page' v-if='places && !places.length'>Pas de place a afficher</div><div class='location-box' v-if='places && places.length' v-for='place in places' :key='place.id'>         <ul class='contains-task-list'><li class='task-list-item'><label><input type='checkbox' @click='toggleVisited(place)' :checked='place.visited'><span> {{place.name}}</span></label></li></ul>      <div><button class='btn-edit'><router-link :to='`/places/${place.id}`'>Edit</router-link></button><button @click='deletePlace(place.id)' class='btn-delete'>Delete</button></div> </div>      <br> ",
+    "<div >Welcome to the {{ location.name }} Page!</div> <br> <br>       <form @submit.prevent='addPlace()'><h2>Add new places</h2> <div style='display:flex;flex-direction: column;'><label for='name'>Place Name</label><input  name='name' v-model='name' type='text' /></div><div class='lat-long'><div><label for='latitude'>Latitude</label><input name='lat' v-model='lat' type='number' /></div><div><label for='latitude'>Longitude</label><input name='lng' v-model='lng' type='number' /></div></div><button class='btn-add' type='submit'>Add new place</button></form>   <br> <br>   <div class='empty-page' v-if='places && !places.length'>Pas de place a afficher</div><div class='location-box' v-if='places && places.length' v-for='place in places' :key='place.id'>         <ul class='contains-task-list'><li class='task-list-item'><label><input type='checkbox' @click='() => toggleVisited(place)' :checked='place.visited'><span> {{place.name}}</span></label></li></ul>      <div><button class='btn-edit'><router-link :to='`/places/${place.id}`'>Edit</router-link></button><button @click='deletePlace(place.id)' class='btn-delete'>Delete</button></div> </div>      <br> ",
   data() {
     return {
       location: "",
@@ -27,6 +27,31 @@ const LocationPage = {
       .catch((error) => console.error(error));
   },
   methods: {
+    async toggleVisited(place) {
+      console.log(place);
+      try {
+        const response = await fetch(`${API_URL}/places/${place.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: place.name,
+            lat: place.lat,
+            lng: place.lng,
+            visited: !place.visited,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        place.visited = !place.visited;
+      } catch (error) {
+        console.error("There was a problem updating the place:", error);
+      }
+    },
     async deletePlace(placeId) {
       try {
         const response = await fetch(`${API_URL}/places/${placeId}`, {
@@ -80,31 +105,6 @@ const LocationPage = {
         console.error("There was a problem adding the place:", error);
       }
     },
-  },
-  async toggleVisited(place) {
-    console.log("working");
-    try {
-      const response = await fetch(`${API_URL}/places/${place.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: place.name,
-          lat: place.lat,
-          lng: place.lng,
-          visited: !place.visited,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      place.visited = !place.visited;
-    } catch (error) {
-      console.error("There was a problem updating the place:", error);
-    }
   },
 };
 
