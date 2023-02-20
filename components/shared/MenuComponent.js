387cc7fbddd46163d4294e9af4e8e20864abac04
CasianLW <ciorbcasian@gmail.com>
2023-02-20 16:41:50 +0100

    correction br menu

--- components/shared/MenuComponent.js
+++ components/shared/MenuComponent.js
@@ -1,6 +1,6 @@
 const MenuComponent = {
   template:
-    '<div style="place-self: center;"><router-link class="btn-menu" to="/">Home</router-link> </div> <br> ',
+    '<div style="place-self: center;"><router-link class="btn-menu" to="/">Home</router-link> </div> ',
 };
 
 export default MenuComponent;
